e67427b958de0ede55bd60e43816c6946c805490
firstRepo <hassanjawedwani@gmail.com>
2024-06-28 04:19:13 +0500

    day32

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+app.listen(port, () => {
+  console.log("Server is listening...");
+})
+
+const path = require("path");
+app.set("views", path.join(__dirname, "/views"));
+
+app.set("view engine", "ejs");
+
+
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+})  
+
+app.get("/about", (req, res) => {
+  res.send("about page");
+})  
+
+app.get("/*", (req, res) => {
+  res.send("4040");
+})  
